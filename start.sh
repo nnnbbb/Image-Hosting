f85b2349f0f8ef2238a2e26c9929906576a7b831
yanxl <923932093@qq.com>
2023-09-21 16:12:00 +0800

    update

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 git pull
 npm i 
 pm2 delete Image-Hosting
-export NODE_ENV=production && pm2 start index.js -i 2 --name=Image-Hosting --log-date-format "YYYY-MM-DD HH:mm:ss"
+export NODE_ENV=production && pm2 start index.js -i 1 --name=Image-Hosting --log-date-format "YYYY-MM-DD HH:mm:ss"
 pm2 logs Image-Hosting
